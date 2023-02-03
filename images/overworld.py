bb58d7c4fbfac15c885264e2ad146640ee8eb51a
sophia kim <sophkim26@bergen.org>
2023-02-03 12:48:58 -0500

    make sprite

--- images/overworld.py
+++ images/overworld.py
@@ -15,6 +15,23 @@ DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
 
+class Block(pygame.sprite.Sprite):
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
 def main():
     
     global BASICFONT, IMAGESDICT, PLAYERIMAGES, window, bg # add global variables
