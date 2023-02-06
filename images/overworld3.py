e604ef34a59bed1a982f70fa9fa6e8536dbadd18
sylviaalee <sylvieelee@gmail.com>
2023-02-06 13:15:42 -0500

    dafasdfdas

--- images/overworld3.py
+++ images/overworld3.py
@@ -3,5 +3,11 @@ import pygame
 pygame.init()
 pygame.display.set_caption('Cat Isle')
 
+# window
+window = pygame.display.set_mode((500, 500))
+bg = pygame.image.load("background.png")
+bg = pygame.transform.scale(bg,(WIDTH, HEIGHT))
+
+# cat
 cat = pygame.image.load('cat_sample.png')
 window.blit(cat, (100, 100))
