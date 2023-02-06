fec9e241fe1ef748ca45e18ea834485bc3c53b5b
sophia kim <sophkim26@bergen.org>
2023-02-06 13:15:28 -0500

    adkfljsafvesd

--- images/overworld3.py
+++ images/overworld3.py
@@ -2,6 +2,6 @@ import pygame
 
 pygame.init()
 pygame.display.set_caption('Cat Isle')
-
+x, y = 100, 100
 cat = pygame.image.load('cat_sample.png')
-window.blit(cat, (100, 100))
+window.blit(cat, (x, y))
