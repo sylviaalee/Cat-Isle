e8a52c223cd40a99bdebc71426d68727a3df40e0
sylviaalee <sylvieelee@gmail.com>
2023-02-07 13:18:46 -0500

    dsf

--- images/overworld3.py
+++ images/overworld3.py
@@ -1,44 +1,34 @@
 import pygame
 
 pygame.init()
-pygame.font.get_fonts()
 
 # GLOBAL
 WIDTH = 1470
 HEIGHT = 850
-BASICFONT = pygame.font.Font('freesansbold.ttf', 25)
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
 
-# add caption
+# add text
 pygame.display.set_caption('Cat Isle')
+text = BASICFONT.render('Cat Isle', True, 'brown')
+textRect = text.get_rect()
+textRect.center = (1470 // 2, 850 // 2)
+
+# window
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+bg = pygame.image.load("background.png")
+bg = pygame.transform.scale(bg,(WIDTH, HEIGHT))
 
 # cat
 cat = pygame.image.load('cat_sample.png')
 cat = pygame.transform.scale(cat, (400, 300))
 x, y = 100, 100
 
-def main_screen():
-    # add text
-    text = BASICFONT.render('Cat Isle', True, 'brown')
-    textRect = text.get_rect()
-    textRect.center = (1470 // 2, 100)
-
-    # window
-    bg = pygame.image.load("background.png")
-    bg = pygame.transform.scale(bg,(WIDTH, HEIGHT))
-
-    window.blit(bg, (0,0)) # create bg
-    window.blit(text, textRect) # create text
-
-    # make portals
-    pygame.draw.circle(window, 'aquamarine4', (100, 100), 50)
-    pygame.draw.circle(window, 'aquamarine4', (1370, 100), 50)
-    pygame.draw.circle(window, 'aquamarine4', (1370, 750), 50)
-    pygame.draw.circle(window, 'aquamarine4', (100, 750), 50)
 
 loop = True
 while loop:
-    window.blit(cat, (x, y)) # spawn cat
+    window.blit(bg, (0,0))
+    window.blit(cat, (x, y))
+    window.blit(text, textRect) # create text
 
     for event in pygame.event.get():
         if event.type == quit:
@@ -53,8 +43,6 @@ while loop:
         y -= 8
     if keys[pygame.K_DOWN] and y < 600:
         y += 8
-    if keys[pygame.K_KP_ENTER]:
-        pass
     pygame.display.flip()
 
 pygame.quit()
