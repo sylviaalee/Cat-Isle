201aa54cfa9209460c73df409ca79264e3a260de
sylviaalee <sylvieelee@gmail.com>
2023-02-07 13:26:15 -0500

    sdf

--- images/overworld3.py
+++ images/overworld3.py
@@ -7,28 +7,32 @@ WIDTH = 1470
 HEIGHT = 850
 BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
 
-# add text
-pygame.display.set_caption('Cat Isle')
-text = BASICFONT.render('Cat Isle', True, 'brown')
-textRect = text.get_rect()
-textRect.center = (1470 // 2, 850 // 2)
+def main_screen():
+    # add text
+    pygame.display.set_caption('Cat Isle')
+    text = BASICFONT.render('Cat Isle', True, 'brown')
+    textRect = text.get_rect()
+    textRect.center = (1470 // 2, 850 // 2)
 
-# window
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-bg = pygame.image.load("background.png")
-bg = pygame.transform.scale(bg,(WIDTH, HEIGHT))
+    # window
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    bg = pygame.image.load("background.png")
+    bg = pygame.transform.scale(bg,(WIDTH, HEIGHT))
+
+    window.blit(bg, (0,0))
+    window.blit(text, textRect) # create text
 
 # cat
 cat = pygame.image.load('cat_sample.png')
 cat = pygame.transform.scale(cat, (400, 300))
 x, y = 100, 100
 
+def terminate():
+    pygame.quit()
 
 loop = True
 while loop:
-    window.blit(bg, (0,0))
     window.blit(cat, (x, y))
-    window.blit(text, textRect) # create text
 
     for event in pygame.event.get():
         if event.type == quit:
@@ -45,4 +49,4 @@ while loop:
         y += 8
     pygame.display.flip()
 
-pygame.quit()
+terminate()
