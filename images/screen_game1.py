6972211d041f6a8ad5407fdcb822d4129e960197
sophia kim <sophkim26@bergen.org>
2023-02-08 12:49:19 -0500

    m

--- images/screen_game1.py
+++ images/screen_game1.py
@@ -12,7 +12,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 def game1():
     # text
     pygame.display.set_caption('Cat Isle')
-    text = BASICFONT.render('Game 1 (Replace with Game Title)', True, 'brown')
+    text = BASICFONT.render('Flappy Cat', True, 'brown')
     textRect = text.get_rect()
     textRect.center = (1470 // 2, 850 // 2)
 
