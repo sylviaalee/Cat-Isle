44ae3cf002b459431024df4b1b491180d21453ce
sophia kim <sophkim26@bergen.org>
2023-02-08 13:12:10 -0500

    d

--- images/screen_game2.py
+++ images/screen_game2.py
@@ -1,4 +1,4 @@
-#from overworld3 import *
+# from overworld3 import *
 import pygame, sys
 import random
 from pygame.locals import *
@@ -40,63 +40,60 @@ def game2():
 
 # fps = pygame.time.Clock()
 
-# # colors
-# WHITE = (255,255,255)
-# RED = (255,0,0)
-# GREEN = (0,255,0)
-# BLACK = (0,0,0)
+# colors
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLACK = (0,0,0)
 
-# # globals
-# WIDTH = 600
-# HEIGHT = 400       
-# BALL_RADIUS = 20
-# PAD_WIDTH = 8
-# PAD_HEIGHT = 80
-# HALF_PAD_WIDTH = PAD_WIDTH / 2
-# HALF_PAD_HEIGHT = PAD_HEIGHT / 2
-# ball_pos = [0,0]
-# ball_vel = [0,0]
-# paddle1_vel = 0
-# paddle2_vel = 0
-# l_score = 0
-# r_score = 0
+# globals
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH / 2
+HALF_PAD_HEIGHT = PAD_HEIGHT / 2
+ball_pos = [0,0]
+ball_vel = [0,0]
+paddle1_vel = 0
+paddle2_vel = 0
+l_score = 0
+r_score = 0
 
 # # canvas declaration
 # window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 # pygame.display.set_caption('Hello World')
 
-# # helper function that spawns a ball, returns a position vector and a velocity vector
-# # if right is True, spawn to the right, else spawn to the left
-# def ball_init(right):
-#     global ball_pos, ball_vel # these are vectors stored as lists
-#     ball_pos = [WIDTH/2,HEIGHT/2]
-#     horz = random.randrange(2,4)
-#     vert = random.randrange(1,3)
+# helper function that spawns a ball, returns a position vector and a velocity vector
+# if right is True, spawn to the right, else spawn to the left
+def ball_init(right):
+    global ball_pos, ball_vel # these are vectors stored as lists
+    ball_pos = [WIDTH/2,HEIGHT/2]
+    horz = random.randrange(2,4)
+    vert = random.randrange(1,3)
     
-#     if right == False:
-#         horz = - horz
+    if right == False:
+        horz = - horz
         
-#     ball_vel = [horz,-vert]
+    ball_vel = [horz,-vert]
 
-# # define event handlers
-# def init():
-#     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score  # these are floats
-#     global score1, score2  # these are ints
-#     paddle1_pos = [HALF_PAD_WIDTH - 1,HEIGHT/2]
-#     paddle2_pos = [WIDTH +1 - HALF_PAD_WIDTH,HEIGHT/2]
-#     l_score = 0
-#     r_score = 0
-#     if random.randrange(0,2) == 0:
-#         ball_init(True)
-#     else:
-#         ball_init(False)
+# define event handlers
+def init():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score  # these are floats
+    global score1, score2  # these are ints
+    paddle1_pos = [HALF_PAD_WIDTH - 1,HEIGHT/2]
+    paddle2_pos = [WIDTH +1 - HALF_PAD_WIDTH,HEIGHT/2]
+    l_score = 0
+    r_score = 0
+    if random.randrange(0,2) == 0:
+        ball_init(True)
+    else:
+        ball_init(False)
 
 
-# #draw function of canvas
+# # draw function of canvas
 # def draw(canvas):
 #     global paddle1_pos, paddle2_pos, ball_pos, ball_vel, l_score, r_score
-           
-#     canvas.fill(BLACK)
+
 #     pygame.draw.line(canvas, WHITE, [WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1)
 #     pygame.draw.line(canvas, WHITE, [PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1)
 #     pygame.draw.line(canvas, WHITE, [WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1)
