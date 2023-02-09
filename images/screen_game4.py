161ab573aef35bbbdb06e85cfa0e5e230b4bb8b9
sophia kim <sophkim26@bergen.org>
2023-02-09 12:44:49 -0500

    catmaze

--- images/screen_game4.py
+++ images/screen_game4.py
@@ -12,7 +12,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 def game4():
     # text
     pygame.display.set_caption('Cat Isle')
-    text = BASICFONT.render('Game 4 (Replace with Game Title)', True, 'brown')
+    text = BASICFONT.render('CatMaze', True, 'brown')
     textRect = text.get_rect()
     textRect.center = (1470 // 2, 850 // 2)
 
