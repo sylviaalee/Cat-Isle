554f51618a247ea7fb83c0ece3b1c0031cd7b9cf
sylviaalee <sylvieelee@gmail.com>
2023-02-22 13:21:05 -0500

    asdf

--- images/screen_game4.py
+++ images/screen_game4.py
@@ -66,69 +66,69 @@ def game4():
                     bx = 0
                     by = by + 1
 
-class App:
- 
-    windowWidth = 800
-    windowHeight = 600
-    player = 0
- 
-    def __init__(self):
-        self._running = True
-        self._display_surf = None
-        self._image_surf = None
-        self._block_surf = None
-        self.player = Player()
-        self.maze = Maze()
- 
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+    class App:
+    
+        windowWidth = 800
+        windowHeight = 600
+        player = 0
+    
+        def __init__(self):
+            self._running = True
+            self._display_surf = None
+            self._image_surf = None
+            self._block_surf = None
+            self.player = Player()
+            self.maze = Maze()
+    
+        def on_init(self):
+            pygame.init()
+            self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+            
+            pygame.display.set_caption('Pygame pythonspot.com example')
+            self._running = True
+            self._image_surf = pygame.image.load("player.png").convert()
+            self._block_surf = pygame.image.load("block.png").convert()
+    
+        def on_event(self, event):
+            if event.type == QUIT:
+                self._running = False
+    
+        def on_loop(self):
+            pass
         
-        pygame.display.set_caption('Pygame pythonspot.com example')
-        self._running = True
-        self._image_surf = pygame.image.load("player.png").convert()
-        self._block_surf = pygame.image.load("block.png").convert()
- 
-    def on_event(self, event):
-        if event.type == QUIT:
-            self._running = False
- 
-    def on_loop(self):
-        pass
+        def on_render(self):
+            self._display_surf.fill((0,0,0))
+            self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
+            self.maze.draw(self._display_surf, self._block_surf)
+            pygame.display.flip()
     
-    def on_render(self):
-        self._display_surf.fill((0,0,0))
-        self._display_surf.blit(self._image_surf,(self.player.x,self.player.y))
-        self.maze.draw(self._display_surf, self._block_surf)
-        pygame.display.flip()
- 
-    def on_cleanup(self):
-        pygame.quit()
- 
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
- 
-        while( self._running ):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    game_over = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        self.player.moveLeft()
-                    elif event.key == pygame.K_RIGHT:
-                        self.player.moveRight()
-                    elif event.key == pygame.K_UP:
-                        self.player.moveUp()
-                    elif event.key == pygame.K_DOWN:
-                        self.player.moveDown()
-                    elif event.key == pygame.K_ESCAPE:
-                        self.running = False
+        def on_cleanup(self):
+            pygame.quit()
     
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
- 
-if __name__ == "__main__" :
-    theApp = App()
-    theApp.on_execute()
+        def on_execute(self):
+            if self.on_init() == False:
+                self._running = False
+    
+            while( self._running ):
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        game_over = True
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_LEFT:
+                            self.player.moveLeft()
+                        elif event.key == pygame.K_RIGHT:
+                            self.player.moveRight()
+                        elif event.key == pygame.K_UP:
+                            self.player.moveUp()
+                        elif event.key == pygame.K_DOWN:
+                            self.player.moveDown()
+                        elif event.key == pygame.K_ESCAPE:
+                            self.running = False
+        
+                self.on_loop()
+                self.on_render()
+            self.on_cleanup()
+    
+    if __name__ == "__main__" :
+        theApp = App()
+        theApp.on_execute()
