c75e550e3026c4e15889e872f3d38cc9d7b6d85b
sophia kim <sophkim26@bergen.org>
2023-02-22 13:13:41 -0500

    mAZE

--- images/screen_game4.py
+++ images/screen_game4.py
@@ -22,3 +22,29 @@ def game4():
 
     window.blit(bg, (0,0))
     window.blit(text, textRect)
+
+     
+    class Maze:
+        def __init__(self):
+            self.M = 10
+            self.N = 8
+            maze = [1,1,1,1,1,1,1,1,0,1,
+                    1,0,0,0,0,0,0,0,0,1,
+                    1,0,0,0,0,0,0,0,0,1,
+                    1,0,1,1,1,1,1,1,0,1,
+                    1,0,1,0,0,0,0,0,0,1,
+                    1,0,1,0,1,1,1,1,0,1,
+                    1,0,0,0,0,0,0,0,0,1,
+                    1,0,1,1,1,1,1,1,1,1,]
+
+        def draw(self,display_surf,image_surf):
+            bx = 0
+            by = 0
+            for i in range(0,self.M*self.N):
+                if self.maze[ bx + (by*self.M) ] == 1:
+                    display_surf.blit(image_surf,( bx * 44 , by * 44))
+        
+                bx = bx + 1
+                if bx > self.M-1:
+                    bx = 0
+                    by = by + 1
