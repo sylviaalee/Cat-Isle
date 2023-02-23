cc41776808f85eadf3b9efb7959c82a7db40f8b0
sophia kim <sophkim26@bergen.org>
2023-02-23 08:21:47 -0500

    d

--- images/screen_game4.py
+++ images/screen_game4.py
@@ -59,7 +59,7 @@ def game4():
             by = 0
             for i in range(0,self.M*self.N):
                 if self.maze[ bx + (by*self.M) ] == 1:
-                    display_surf.blit(image_surf,( bx * 44 , by * 44))
+                    display_surf.blit(image_surf,( bx * 10 , by * 10))
         
                 bx = bx + 1
                 if bx > self.M-1:
