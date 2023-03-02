51216f30f21d30db4c04da3f270a4d589f93db60
sophia kim <sophkim26@bergen.org>
2023-03-02 08:22:31 -0500

    d

--- images/overworld3.py
+++ images/overworld3.py
@@ -94,19 +94,19 @@ while loop:
             loop = False
     
     # trophy won?
-    if screen_game1.trophy1:
+    if screen_game1.trophy1 and SCREEN == 'main':
         trophy = pygame.image.load('game1_spring.png')
         trophy = pygame.transform.scale(trophy, (65, 65))
         window.blit(trophy, (970, 697))
-    if screen_game2.trophy2:
+    if screen_game2.trophy2 and SCREEN == 'main':
         trophy = pygame.image.load('game2_summer.png')
         trophy = pygame.transform.scale(trophy, (65, 65))
         window.blit(trophy, (1034, 697))
-    if screen_game3.trophy3:
+    if screen_game3.trophy3 and SCREEN == 'main':
         trophy = pygame.image.load('game3_fall.png')
         trophy = pygame.transform.scale(trophy, (65, 65))
         window.blit(trophy, (1093, 697))
-    if screen_game4.trophy4:
+    if screen_game4.trophy4 and SCREEN == 'main':
         trophy = pygame.image.load('game4_winter.png')
         trophy = pygame.transform.scale(trophy, (65, 65))
         window.blit(trophy, (1153, 697))
