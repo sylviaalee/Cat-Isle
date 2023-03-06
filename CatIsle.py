5274fc173cdc223951587e8a220d4bc22f209c65
sophia kim <sophkim26@bergen.org>
2023-03-06 13:17:12 -0500

    d

--- CatIsle.py
+++ CatIsle.py
@@ -964,7 +964,7 @@ def game4():
 
     # text
     pygame.display.set_caption('Cat Isle')
-    text = BASICFONT.render('HangCat?', True, 'brown')
+    text = BASICFONT.render('HangCat', True, 'brown')
     textRect = text.get_rect()
     textRect.center = (1470 // 2, 850 // 2)
 
@@ -1433,25 +1433,25 @@ def loop():
 
         # display directions if cat is on portal
         if x < -5 and x > -300 and y < 15 and y > -300 and SCREEN == "main":
-            game1_instruct = BASICFONT.render('Press ENTER to go to game 1', True, 'brown')
+            game1_instruct = BASICFONT.render('Press ENTER to go to the Spring Mini-Game (#1)', True, 'brown')
             rect1 = game1_instruct.get_rect()
             rect1.center = (1470 // 2, 30)
             window.blit(game1_instruct, rect1)
 
         if x < 2000 and x > 1100 and y < 30 and y > -200 and SCREEN == "main":
-            game3_instruct = BASICFONT.render('Press ENTER to go to game 2', True, 'brown')
+            game3_instruct = BASICFONT.render('Press ENTER to go to the Summer Mini-Game (#2), True, 'brown')
             rect3 = game3_instruct.get_rect()
             rect3.center = (1470 // 2, 30)
             window.blit(game3_instruct, rect3)    
 
         if x < 2000 and x > 1100 and y < 2000 and y > 475 and SCREEN == "main":
-            game2_instruct = BASICFONT.render('Press ENTER to go to game 3', True, 'brown')
+            game2_instruct = BASICFONT.render('Press ENTER to go to the Autumn Mini-Game (#3)', True, 'brown')
             rect2 = game2_instruct.get_rect()
             rect2.center = (1470 // 2, 30)
             window.blit(game2_instruct, rect2)
 
         if x < -5 and x > -300 and y < 2000 and y > 475 and SCREEN == "main":
-            game4_instruct = BASICFONT.render('Press ENTER to go to game 4', True, 'brown')
+            game4_instruct = BASICFONT.render('Press ENTER to go to the Winter Mini-Game (#4)', True, 'brown')
             rect4 = game4_instruct.get_rect()
             rect4.center = (1470 // 2, 30)
             window.blit(game4_instruct, rect4)
