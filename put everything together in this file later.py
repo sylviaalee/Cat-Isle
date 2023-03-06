117ace4e88b150790796db918bca787e3fcde898
sylviaalee <sylvieelee@gmail.com>
2023-03-05 19:25:26 -0500

    asdfds

--- put everything together in this file later.py
+++ put everything together in this file later.py
@@ -1,5 +1,4 @@
 import pygame, random, time, screen_game1, screen_game2, screen_game3, screen_game4
-import cv2
 from pygame import mixer
 pygame.init()
 mixer.init()
@@ -1210,28 +1209,7 @@ def game4():
     main_loop4()
 
 def collected_all_trophies():
-    video = cv2.VideoCapture("video.mp4")
-    success, video_image = video.read()
-    fps = video.get(cv2.CAP_PROP_FPS)
-
-    window = pygame.display.set_mode(video_image.shape[1::-1])
-    clock = pygame.time.Clock()
-
-    run = success
-    while run:
-        clock.tick(fps)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        
-        success, video_image = video.read()
-        if success:
-            video_surf = pygame.image.frombuffer(
-                video_image.tobytes(), video_image.shape[1::-1], "BGR")
-        else:
-            run = False
-        window.blit(video_surf, (0, 0))
-        pygame.display.flip()        
+    pass        
 
 # function for MAIN SCREEN
 def main_screen():
